7fb21ce6a9b266b2b11f5b8638c7d09048e4c765
vincentpaulines <154640145+vincentpaulines@users.noreply.github.com>
2023-12-22 18:17:03 -0800

    Update password.js

--- password.js
+++ password.js
@@ -1,12 +1,17 @@
 function getValue() {
-	let givenanswer = eval(document.inputbox.elements[0].value)
-	let trueanswer = 71/6
-	if (givenanswer == trueanswer) {
+	if (document.inputbox.elements[0].value == "shrimp") {
 		console.log("true")
 		window.location = "https://chillsome.carrd.co/#literallyasuka"
 		event.preventDefault()
 	} else {
-		console.log("false")
-	} 
+		let givenanswer = eval(document.inputbox.elements[0].value)
+		let trueanswer = 71/6
+		if (givenanswer == trueanswer) {
+			console.log("true")
+			window.location = "https://chillsome.carrd.co/#literallyasuka"
+			event.preventDefault()
+		} else {
+			console.log("false")
+		} 
+	}
 }
-
